25620093bcd07b1f9d80b5266c60335783793ec6
Lolodusiji <sholaodusiji@gmail.com>
2023-08-21 14:27:02 +0100

    fixes

--- src/pages/sidebar/SideBarLink.jsx
+++ src/pages/sidebar/SideBarLink.jsx
@@ -1,62 +1,62 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import { BiSearchAlt2 } from "react-icons/bi";
-import "./Sidebar.css";
+// import React from 'react'
+// import {NavLink} from 'react-router-dom'
+// import { BiSearchAlt2 } from "react-icons/bi";
+// import "./Sidebar.css";
 
-const SideBarLink = () => {
-  return (
-    <div className='link_container'>
-    <NavLink to='/' className={({ isActive }) => isActive ? 'link active' : 'link'}>
-     <BiSearchAlt2
-                    style={{ transform: "rotate(90deg)" }}
-                    size={25}
-                  />
-                  <p>Discovery</p>
-    </NavLink>
-    <NavLink to='/dashboard' className={({ isActive }) => isActive ? 'link active' : 'link'}>
-     <BiSearchAlt2
-                    style={{ transform: "rotate(90deg)" }}
-                    size={25}
-                  />
-                  <p>Dashboard</p>
-    </NavLink>
-    <NavLink to='/social' className={({ isActive }) => isActive ? 'link active' : 'link'}>
-     <BiSearchAlt2
-                    style={{ transform: "rotate(90deg)" }}
-                    size={25}
-                  />
-                  <p>Social</p>
-    </NavLink>
-    <NavLink to='/segmentation' className={({ isActive }) => isActive ? 'link active' : 'link'}>
-     <BiSearchAlt2
-                    style={{ transform: "rotate(90deg)" }}
-                    size={25}
-                  />
-                  <p>Segmentaition</p>
-    </NavLink>
-    <NavLink to='/competitors' className={({ isActive }) => isActive ? 'link active' : 'link'}>
-     <BiSearchAlt2
-                    style={{ transform: "rotate(90deg)" }}
-                    size={25}
-                  />
-                  <p>Competitors</p>
-    </NavLink>
-    <NavLink to='/channels' className={({ isActive }) => isActive ? 'link active' : 'link'}>
-     <BiSearchAlt2
-                    style={{ transform: "rotate(90deg)" }}
-                    size={25}
-                  />
-                  <p>Channels</p>
-    </NavLink>
-    <NavLink to='/support' className={({ isActive }) => isActive ? 'link active' : 'link'}>
-     <BiSearchAlt2
-                    style={{ transform: "rotate(90deg)" }}
-                    size={25}
-                  />
-                  <p>Support</p>
-    </NavLink>
-    </div>
-  )
-}
+// const SideBarLink = () => {
+//   return (
+//     <div className='link_container'>
+//     <NavLink to='/' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+//      <BiSearchAlt2
+//                     style={{ transform: "rotate(90deg)" }}
+//                     size={25}
+//                   />
+//                   <p>Discovery</p>
+//     </NavLink>
+//     <NavLink to='/dashboard' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+//      <BiSearchAlt2
+//                     style={{ transform: "rotate(90deg)" }}
+//                     size={25}
+//                   />
+//                   <p>Dashboard</p>
+//     </NavLink>
+//     <NavLink to='/social' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+//      <BiSearchAlt2
+//                     style={{ transform: "rotate(90deg)" }}
+//                     size={25}
+//                   />
+//                   <p>Social</p>
+//     </NavLink>
+//     <NavLink to='/segmentation' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+//      <BiSearchAlt2
+//                     style={{ transform: "rotate(90deg)" }}
+//                     size={25}
+//                   />
+//                   <p>Segmentaition</p>
+//     </NavLink>
+//     <NavLink to='/competitors' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+//      <BiSearchAlt2
+//                     style={{ transform: "rotate(90deg)" }}
+//                     size={25}
+//                   />
+//                   <p>Competitors</p>
+//     </NavLink>
+//     <NavLink to='/channels' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+//      <BiSearchAlt2
+//                     style={{ transform: "rotate(90deg)" }}
+//                     size={25}
+//                   />
+//                   <p>Channels</p>
+//     </NavLink>
+//     <NavLink to='/support' className={({ isActive }) => isActive ? 'link active' : 'link'}>
+//      <BiSearchAlt2
+//                     style={{ transform: "rotate(90deg)" }}
+//                     size={25}
+//                   />
+//                   <p>Support</p>
+//     </NavLink>
+//     </div>
+//   )
+// }
 
-export default SideBarLink
+// export default SideBarLink
